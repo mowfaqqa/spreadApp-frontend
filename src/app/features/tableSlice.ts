107da6c3437a44e5d3d52fe4384951b72b2205aa
muwaffaq <itzmuwaffaq@gmail.com>
2023-06-12 01:01:03 +0100

    modified table splice to dynamically add columns

--- src/app/features/tableSlice.ts
+++ src/app/features/tableSlice.ts
@@ -5,10 +5,12 @@ type TableRow  = {
 };
 
 type TableState = {
+  columns: string[];
   rows: TableRow[];
 }
 
 const initialState: TableState = {
+  columns: [],
   rows: [],
 }
 
@@ -16,15 +18,35 @@ const tableSlice = createSlice({
   name: 'table',
   initialState,
   reducers: {
+    addColumn: (state, action: PayloadAction<string>) => {
+      state.columns.push(action.payload);
+      state.rows.forEach((row) => {
+        row[action.payload]= '';
+      });
+    },
+    removeColumn: (state, action: PayloadAction<string>) => {
+      const columnToRemove = action.payload;
+      state.columns = state.columns.filter((column) => column !== columnToRemove);
+      state.rows.forEach((row) => {
+        delete row[columnToRemove];
+      })
+    },
     addRow: (state) => {
+      const newRow: TableRow = {};
+      state.columns.forEach((column) => {
+        newRow[column] = '';
+      });
       state.rows.push({});
     },
-    updateCell: (state, action: PayloadAction<{rowIndex: number; column: string; value: string }>) => {
+    updateCell: (
+      state,
+      action: PayloadAction<{ rowIndex: number; column: string; value: string }>
+    ) => {
       const { rowIndex, column, value } = action.payload;
-      state.rows[rowIndex] = {...state.rows[rowIndex], [column]: value};
-    }
+      state.rows[rowIndex][column] = value;
+    },
   }
 })
 
-export const {addRow, updateCell} = tableSlice.actions;
+export const { addColumn, removeColumn, addRow, updateCell } = tableSlice.actions;
 export default tableSlice.reducer
